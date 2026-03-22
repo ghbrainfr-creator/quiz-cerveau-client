26d4666265f4e545f3efc68d8bf8185e95fd9932
ghbrainfr-creator <ghbrainfr@gmail.com>
2026-03-22 21:27:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import QuizCerveauClient from './QuizCerveauClient.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <QuizCerveauClient />
-    </React.StrictMode>React.StrictMode>
-  )</React.StrictMode>
+  <React.StrictMode>
+    <QuizCerveauClient />
+  </React.StrictMode>
+)
