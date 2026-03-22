de44d5aa1e309cf47eff02048e5daf45fbf5db81
ghbrainfr-creator <ghbrainfr@gmail.com>
2026-03-22 20:24:55 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QuizCerveauClient from './QuizCerveauClient.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <QuizCerveauClient />
+    </React.StrictMode>React.StrictMode>
+  )</React.StrictMode>
